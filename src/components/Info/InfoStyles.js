9490675b66c6314a48dd15b2a80851eb7d331e52
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2022-04-16 21:43:51 +0530

    Merge Development to Master (#2)
    * Introduced the Info Section, Footer (#1)
    
    * Update README.md

---
+++ src/components/Info/InfoStyles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const InfoTitle = styled.h2`
+    text-align: center;
+    margin-top: 5%;
+    font-weight: bold;
+
+`
+export const Card = styled.div`
+    width: "18rem";
+    border: "none";
+
+    :hover{
+        cursor: pointer;
+        box-shadow: 0 0 35px -2px rgb(0 0 0 / 20%);
+    }
+`
