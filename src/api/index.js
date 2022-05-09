5b36d65bbd748247164788a0ef378b02eaf7a240
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2022-05-09 12:02:18 +0530

    Exit Trade Was Implemented Successfully

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@ const env = {
 }
 
 const API = axios.create({
-    baseURL: env["prod"]
+    baseURL: env["local"]
 });
 
 // setting auth header
@@ -27,3 +27,4 @@ export const logout = () => API.post('/logout')
 // TRADE 
 export const allTrades = () => API.get("/all_trades")
 export const enterTrade = (tradeData) => API.post("/enter_trade", tradeData)
+export const exitTrade = (tradeData) => API.post("/exit_trade", tradeData)
