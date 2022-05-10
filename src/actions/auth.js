499f0c8628d4532f7f6889aff13a75ac50b390d3
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2022-05-10 14:43:11 +0530

    Forget Password Completed (#13)
    * Introduced the Info Section, Footer (#1)
    
    * Update README.md
    
    * New trade (#3)
    
    * Introduced the Info Section, Footer
    
    * Merge Development to Master (#2)
    
    * Introduced the Info Section, Footer (#1)
    
    * Update README.md
    
    * Updated README.md file
    
    * Updated title and fav icon
    
    * Made the latest trade table
    
    * Wrote API request to fetch all the trades, need to figure out how to call it efficiently on startup
    
    * Completed the New Trade Modal and Loading for login
    
    * Settled the toaster issue temporarily
    
    * Cleaned the code
    
    * BUGFIX: Solved the rendering problem of get all trades function
    
    * changed the environment from local to prod
    
    * Logout bug (#6)
    
    * Development (#4)
    
    * Introduced the Info Section, Footer (#1)
    
    * Update README.md
    
    * New trade (#3)
    
    * Introduced the Info Section, Footer
    
    * Merge Development to Master (#2)
    
    * Introduced the Info Section, Footer (#1)
    
    * Update README.md
    
    * Updated README.md file
    
    * Updated title and fav icon
    
    * Made the latest trade table
    
    * Wrote API request to fetch all the trades, need to figure out how to call it efficiently on startup
    
    * Completed the New Trade Modal and Loading for login
    
    * Settled the toaster issue temporarily
    
    * Cleaned the code
    
    * BUGFIX: Solved the rendering problem of get all trades function
    
    * changed the environment from local to prod
    
    * BIGFIX: Resolved the Logout issue on session token expiry
    
    * Environment changed from local to prod
    
    * ENV CHANGE
    
    * Exit trade (#8)
    
    * Exit Trade Was Implemented Successfully
    
    * * Show/Hide Password
    * Improved Login Experience
    
    * ENV
    
    * Changed Fav Icon
    
    * Forget password Completed (#11)
    
    * Forget Password Functionality Completed
    
    * ENV
    
    * Forget password (#12)
    
    * Forget Password Functionality Completed
    
    * ENV
    
    * Updated the UI exp and error display

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -82,3 +82,100 @@ export const register = (formData , navigate) => async(dispatch) => {
         }
     }
 }
+export const forgetPass = (email) => async(dispatch) => {
+    try{
+        const { data } = await api.forgetPassword(email);
+
+        if( data["success"]){
+            const action = {
+                type: REGISTER,
+                payload: data["data"]
+            }
+    
+            dispatch(action)
+
+            return {
+                "status" : true,
+                "message": data["data"]["msg"]
+            }
+            // navigate('/')
+        }else{
+            return {
+                "status" : false,
+                "message" : data["error"] // when success if false, we have error. Look Backend API
+            }
+        }
+
+    }catch(error){
+        console.log(error.message);
+        return {
+            "status" : false,
+            "message" : "Something went wrong"
+        }
+    }
+}
+export const confirmResetPass = (user_id) => async(dispatch) => {
+    try{
+        const { data } = await api.confirmResetPassword(user_id);
+
+        if( data["success"]){
+            const action = {
+                type: REGISTER,
+                payload: data["data"]
+            }
+    
+            dispatch(action)
+
+            return {
+                "status" : true,
+                "email": data["data"]["email"]
+            }
+            // navigate('/')
+        }else{
+            return {
+                "status" : false,
+                "message" : data["error"] // when success if false, we have error. Look Backend API
+            }
+        }
+
+    }catch(error){
+        console.log(error.message);
+        return {
+            "status" : false,
+            "message" : "Something went wrong"
+        }
+    }
+}
+export const resetPass = (formData) => async(dispatch) => {
+    try{
+        const { data } = await api.resetPassword(formData);
+
+        if( data["success"]){
+            const action = {
+                type: REGISTER,
+                payload: data["data"]
+            }
+    
+            dispatch(action)
+
+            return {
+                "status" : true,
+                "email": data["data"]["msg"]
+            }
+            // navigate('/')
+        }else{
+            return {
+                "status" : false,
+                "message" : data["error"] // when success if false, we have error. Look Backend API
+            }
+        }
+
+    }catch(error){
+        console.log(error.message);
+        return {
+            "status" : false,
+            "message" : "Something went wrong"
+        }
+    }
+}
+
---
+++ src/components/ForgetPassword/ForgetPassword.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import { MainDiv, GreenButton, Heading } from "./ForgetPasswordStyles"
+import { forgetPass } from "../../actions/auth"
+import { useDispatch } from "react-redux";
+
+// OTHERS
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const ForgetPassword = () => {
+
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState("")
+    const [loading, setLoading] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("")
+
+    const success = () => toast.success('🦄 Password Reset email sent successfully. Checkout your email to proceed.', {
+        position: "bottom-right",
+        autoClose: false,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+    
+    const handelChange = (e) => {
+        setErrorMessage("")
+        setShowErrorMessage(false)
+        setEmail(e.target.value)
+    }
+
+    const handleSubmit = async(e) => {
+        console.log(email);
+        e.preventDefault()
+        setLoading(true);
+
+        const response = await dispatch(forgetPass({"email": email}));
+        if(response["status"]){
+            setEmail("");
+            success();
+        }else{
+            setErrorMessage(response["message"])
+            setShowErrorMessage(true);
+        }
+
+        setLoading(false);
+    }
+    return (
+        <>
+            <MainDiv>
+            <Heading className="mb-5">Forget Password</Heading>
+                <div class="card" style={{ width: "22rem" }}>
+                    <div class="card-body">
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email address to reset your password</label>
+                                <input type="email" value={email} onChange={handelChange} className="form-control" name="email"/>
+                            </div>
+                            <div className="d-flex justify-content-center">
+                                {
+                                    loading
+                                    ? (
+                                        <div className="spinner-grow spinner-grow-sm text-success" role="status">
+                                                <span className="visually-hidden">Loading...</span>
+                                            </div>
+                                    )
+                                    : <GreenButton>Submit</GreenButton>
+                                }
+                            </div>
+                            {
+                                showErrorMessage
+                                    ? (
+                                        <span style={{ display: "flex", justifyContent: "center", color: 'red', fontSize: "1rem" }}>
+                                            {errorMessage}
+                                        </span>
+                                    )
+                                    : (<></>)
+                            }
+                        </form>
+                        
+                    </div>
+                </div>
+            </MainDiv>
+
+            <ToastContainer
+                position="bottom-right"
+                autoClose={false}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </>
+    )
+}
+
+export default ForgetPassword
