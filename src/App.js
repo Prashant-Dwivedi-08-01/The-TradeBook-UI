d9ae2e8dc3ea8449139ce92654d95e2211894eda
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2022-06-12 23:36:08 +0530

    Completed Trade Details Section (#14)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero/Hero"
 import ForgetPassword from "./components/ForgetPassword/ForgetPassword"
 import ResetPassword from "./components/ResetPassword/ResetPassword"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TradeDetails from './components/TradeDetails/TradeDetails';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Hero/>} />
         <Route path="/forget-password" element={<ForgetPassword/>} />
         <Route path="/reset-password/:user_id" element={<ResetPassword/>} />
+        <Route path="/details/:script" element={<TradeDetails/>} />
       </Routes>
     </BrowserRouter>
   );
